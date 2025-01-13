f063b16a3903e646b25e3595de48aaad9992558f
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2025-01-13 17:41:24 +0100

    feat: enable dynamic rendering for login and register pages

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,6 +4,8 @@ import { db } from "@/server/db";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function RegisterPage() {
   const userAlreadyCreated = await db.user.findFirst();
 
